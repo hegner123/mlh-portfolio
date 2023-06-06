754f9ffb344182e34452e5e6e9da3cf41d0546d6
Michael Hegner <hegner123@gmail.com>
2023-06-06 18:39:46 -0500

    updated svg and format

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -1,22 +1,16 @@
 import Image from "next/image";
+import ComputerIcon from "../iconComponents/computer";
 export default function Hero() {
   return (
-    <section className="grid grid-cols-12 col-span-full max-h-fit py-[10rem]">
-      <div className="col-span-3 col-start-2 grid items-center">
+    <section className="grid grid-cols-12 col-span-full max-h-fit  ">
+      <ComputerIcon />
+      <div className="col-span-3 col-start-2 grid items-center hero-section">
         <div>
           <h2 className="text-6xl">Michael Hegner</h2>
           <p className="text-md">Full stack developer.</p>
         </div>
       </div>
-      <div className="col-span-6 col-start-7">
-        <Image
-          src="https://via.placeholder.com/500"
-          alt="placeholder"
-          width={500}
-          height={500}
-          priority
-        />
-      </div>
+      <div className="col-span-6 col-start-7">{/* <ComputerIcon /> */}</div>
     </section>
   );
 }
