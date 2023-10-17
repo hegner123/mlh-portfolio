3b8aa7a06a2c280f81480dc8ef69e66263aede82
Michael Hegner <hegner123@gmail.com>
2023-10-17 12:56:36 -0500

    deploy

--- components/about/index.tsx
+++ components/about/index.tsx
@@ -4,8 +4,6 @@ import { TinaMarkdown } from "tinacms/dist/rich-text";
 export default function About(content: any) {
   const aboutTitle = content.content.aboutTitle;
   const aboutBio = content.content.aboutBio;
-
-  console.log(aboutBio.children[0].type);
   return (
     <div className="col-span-10 col-start-2">
       <h3 className="text-4xl text-white">{aboutTitle}</h3>
--- components/projects/index.tsx
+++ components/projects/index.tsx
@@ -4,48 +4,49 @@ import { TinaMarkdown } from "tinacms/dist/rich-text";
 export default function Projects(content: any) {
   const projectsTitle = content.content.projectsTitle;
   const projectItems = content.content.project;
-  console.log(projectItems);
+
   return (
     <article className="grid items-center col-span-full section">
       <div className="grid grid-cols-12 ">
         <div className="col-span-6 col-start-2">
           <h2 className="text-6xl">{projectsTitle}</h2>
         </div>
-        <div className="grid grid-cols-12 col-span-full">
-          <div className="grid col-span-10 col-start-2 gap-4 md:grid-cols-1 lg:grid-cols-2">
-            {projectItems.map((item: any) => (
-              <>
-                <div className="col-start-1 mt-14" key={item.title}>
-                  <h3 className="text-4xl ">{item.title}</h3>
-                  <div className="mt-5 richTextWrapper text-md">
-                    <TinaMarkdown content={item.description} />
-                  </div>
-                  <div className="flex gap-4">
-                    <a
-                      className="block col-start-1 mt-5 text-lg border-b-2 border-b-blue-200 w-fit hover:border-b-blue-400"
-                      href={item.siteLink}>
-                      Site
-                    </a>
-                    <a
-                      className="block col-start-1 mt-5 text-lg border-b-2 border-b-blue-200 w-fit hover:border-b-blue-400"
-                      href={item.githubLink}>
-                      Github
-                    </a>
-                  </div>
+        <ul className="grid grid-cols-12 col-span-full">
+          {projectItems.map((item: any) => (
+            <li
+              className="grid col-span-10 col-start-2 gap-4 md:grid-cols-1 lg:grid-cols-2"
+              key={item.title}>
+              <div className="col-start-1 mt-14">
+                <h3 className="text-4xl ">{item.title}</h3>
+                <div className="mt-5 richTextWrapper text-md">
+                  <TinaMarkdown content={item.description} />
                 </div>
-                <div className="relative col-start-2 mt-14">
-                  <Image
-                    src={item.image}
-                    alt="budget"
-                    width={1600}
-                    height={800}
-                    className="object-cover"
-                  />
+                <div className="flex gap-4">
+                  <a
+                    className="block col-start-1 mt-5 text-lg border-b-2 border-b-blue-200 w-fit hover:border-b-blue-400"
+                    href={item.siteLink}
+                    target="_blank">
+                    Site
+                  </a>
+                  <a
+                    className="block col-start-1 mt-5 text-lg border-b-2 border-b-blue-200 w-fit hover:border-b-blue-400"
+                    href={item.githubLink}
+                    target="_blank">
+                    Github
+                  </a>
                 </div>
-              </>
-            ))}
-          </div>
-        </div>
+              </div>
+              <div className="relative col-start-2 mt-14">
+                <Image
+                  src={item.image}
+                  alt="budget"
+                  fill={true}
+                  className="object-cover"
+                />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </article>
   );
